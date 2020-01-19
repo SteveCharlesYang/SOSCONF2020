75cb9b20c73a34bb0e90ee3b3968c9a86b17ff16
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-19 06:25:22 +0000

    Deploy SteveCharlesYang/SOSCONF2020 to github.com/SteveCharlesYang/SOSCONF2020.git:gh-pages

---
+++ precache-manifest.9e75559463874f4a3298e691903014e4.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db14a05f64e41b1d4883",
+    "url": "/css/app.1936dfa0.css"
+  },
+  {
+    "revision": "e6155af9f6d5d850e8cd",
+    "url": "/css/chunk-31f8bd26.b6811ce9.css"
+  },
+  {
+    "revision": "762b31b37164968c7baa",
+    "url": "/css/chunk-f6bacd34.21dd8b83.css"
+  },
+  {
+    "revision": "f19053533a31bb061d18",
+    "url": "/css/chunk-vendors.021f493c.css"
+  },
+  {
+    "revision": "716c97a7a8188f939a1efb93f1e30503",
+    "url": "/css/material-kit.css"
+  },
+  {
+    "revision": "28bd048c349862df371e4ea4dc44def6",
+    "url": "/css/material-kit.min.css"
+  },
+  {
+    "revision": "82afbfaa2a64cebfd0077f73abe15659",
+    "url": "/img/apple-icon.png"
+  },
+  {
+    "revision": "50f15a1c62e8f493400f0882130f40f3",
+    "url": "/img/bg.jpg"
+  },
+  {
+    "revision": "1813f9d645d23e27edd233139d151be4",
+    "url": "/img/bg2.jpg"
+  },
+  {
+    "revision": "7f7796d84a07cfc2f47cfe5a6477f147",
+    "url": "/img/bg3.jpg"
+  },
+  {
+    "revision": "013b04881a6177ff0648349533ef0447",
+    "url": "/img/bg7.jpg"
+  },
+  {
+    "revision": "58a713c73886bac25ac6aedcbb13de5a",
+    "url": "/img/city-profile.jpg"
+  },
+  {
+    "revision": "98a5d076b890bdd9621049e019253e62",
+    "url": "/img/city.jpg"
+  },
+  {
+    "revision": "996d8248f580f8e26e6c45c67da9b5a6",
+    "url": "/img/favicon.png"
+  },
+  {
+    "revision": "498c5fe9028c8cf85347dbf804295817",
+    "url": "/img/landing.jpg"
+  },
+  {
+    "revision": "c2df5783db76e0f916b77e57f6d975ad",
+    "url": "/img/logo.c2df5783.png"
+  },
+  {
+    "revision": "325a4a07d2dc9b387a582726b7cb4524",
+    "url": "/img/map_icon.325a4a07.png"
+  },
+  {
+    "revision": "5c6521b9bb9cd0c10c3aff8ef1ed1163",
+    "url": "/img/profile.jpg"
+  },
+  {
+    "revision": "682b695a47b802a96c8776114a2c261b",
+    "url": "/img/profile_city.jpg"
+  },
+  {
+    "revision": "5a11bb36715e46bc309a263733fe1a37",
+    "url": "/img/responsive-nav.gif"
+  },
+  {
+    "revision": "7e6ffe55292fa16dd9fa349248dcb856",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db14a05f64e41b1d4883",
+    "url": "/js/app.6e3a36fc.js"
+  },
+  {
+    "revision": "0d4005731ce405a20f61",
+    "url": "/js/chunk-2d22d746.069fc28c.js"
+  },
+  {
+    "revision": "e6155af9f6d5d850e8cd",
+    "url": "/js/chunk-31f8bd26.adad44fc.js"
+  },
+  {
+    "revision": "762b31b37164968c7baa",
+    "url": "/js/chunk-f6bacd34.8064fe6d.js"
+  },
+  {
+    "revision": "f19053533a31bb061d18",
+    "url": "/js/chunk-vendors.ed68e920.js"
+  },
+  {
+    "revision": "479f5022008b8d4b6ef6f2d406ead5cf",
+    "url": "/js/core/bootstrap-material-design.min.js"
+  },
+  {
+    "revision": "986d2baef41aa37ae02ab33355413b98",
+    "url": "/js/core/jquery.min.js"
+  },
+  {
+    "revision": "10a554dd975faf4004fc557d7cf8c998",
+    "url": "/js/core/popper.min.js"
+  },
+  {
+    "revision": "140402aaa6b688b19f414c6cdc5a71b5",
+    "url": "/js/material-kit.js"
+  },
+  {
+    "revision": "260d912aaf83fecdea077916d9c9b18b",
+    "url": "/js/material-kit.min.js"
+  },
+  {
+    "revision": "14382219d09a6c2272e95a95a6bbc5c8",
+    "url": "/js/plugins/bootstrap-datetimepicker.js"
+  },
+  {
+    "revision": "9901cb7dc7a9bce08e8f37fa38891012",
+    "url": "/js/plugins/jquery.sharrre.js"
+  },
+  {
+    "revision": "234c4f6c148398f299e8ba0911e2fc3b",
+    "url": "/js/plugins/moment.min.js"
+  },
+  {
+    "revision": "e22ce95f494c1b97c1215213c03fcd75",
+    "url": "/js/plugins/nouislider.min.js"
+  },
+  {
+    "revision": "4605efb74309679d68949eab001b4fe7",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
