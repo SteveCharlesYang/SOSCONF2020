595a03d1d6c232680ba51dcc090ccc5fef64b1d2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-20 08:42:53 +0000

    Deploy SteveCharlesYang/SOSCONF2020 to github.com/SteveCharlesYang/SOSCONF2020.git:gh-pages

---
+++ precache-manifest.2d2a52d7992f730f93d520434d1fd51e.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b21ac1c082c8a9a1a95",
+    "url": "/css/app.1936dfa0.css"
+  },
+  {
+    "revision": "3f45e79d1f21ab360cef",
+    "url": "/css/chunk-31f8bd26.67f08d2b.css"
+  },
+  {
+    "revision": "ee56fae738769b6a9f93",
+    "url": "/css/chunk-dfc73350.fac2834a.css"
+  },
+  {
+    "revision": "682d2df7621fe4ab3c0b",
+    "url": "/css/chunk-vendors.021f493c.css"
+  },
+  {
+    "revision": "716c97a7a8188f939a1efb93f1e30503",
+    "url": "/css/material-kit.css"
+  },
+  {
+    "revision": "28bd048c349862df371e4ea4dc44def6",
+    "url": "/css/material-kit.min.css"
+  },
+  {
+    "revision": "67a192bcd9f409b01929f3cfb597f6f5",
+    "url": "/data/events.json"
+  },
+  {
+    "revision": "73e8df21e23e78827c62b270a7a85628",
+    "url": "/img/apple-icon.png"
+  },
+  {
+    "revision": "50f15a1c62e8f493400f0882130f40f3",
+    "url": "/img/bg.jpg"
+  },
+  {
+    "revision": "1813f9d645d23e27edd233139d151be4",
+    "url": "/img/bg2.jpg"
+  },
+  {
+    "revision": "7f7796d84a07cfc2f47cfe5a6477f147",
+    "url": "/img/bg3.jpg"
+  },
+  {
+    "revision": "013b04881a6177ff0648349533ef0447",
+    "url": "/img/bg7.jpg"
+  },
+  {
+    "revision": "58a713c73886bac25ac6aedcbb13de5a",
+    "url": "/img/city-profile.jpg"
+  },
+  {
+    "revision": "98a5d076b890bdd9621049e019253e62",
+    "url": "/img/city.jpg"
+  },
+  {
+    "revision": "73e8df21e23e78827c62b270a7a85628",
+    "url": "/img/favicon.png"
+  },
+  {
+    "revision": "498c5fe9028c8cf85347dbf804295817",
+    "url": "/img/landing.jpg"
+  },
+  {
+    "revision": "7bc3142b46913e9efe876ddd82e4d654",
+    "url": "/img/logo.7bc3142b.png"
+  },
+  {
+    "revision": "5c6521b9bb9cd0c10c3aff8ef1ed1163",
+    "url": "/img/profile.jpg"
+  },
+  {
+    "revision": "682b695a47b802a96c8776114a2c261b",
+    "url": "/img/profile_city.jpg"
+  },
+  {
+    "revision": "5a11bb36715e46bc309a263733fe1a37",
+    "url": "/img/responsive-nav.gif"
+  },
+  {
+    "revision": "edd5f7107c1aa097d227efde34fb0d62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b21ac1c082c8a9a1a95",
+    "url": "/js/app.15dfb11c.js"
+  },
+  {
+    "revision": "c140e46abb2a5278db0d",
+    "url": "/js/chunk-2d22c6c5.a39e2183.js"
+  },
+  {
+    "revision": "e50d306c88d814f63b5f",
+    "url": "/js/chunk-2d22d746.35548de8.js"
+  },
+  {
+    "revision": "3f45e79d1f21ab360cef",
+    "url": "/js/chunk-31f8bd26.f8c20e5e.js"
+  },
+  {
+    "revision": "ee56fae738769b6a9f93",
+    "url": "/js/chunk-dfc73350.1fae3538.js"
+  },
+  {
+    "revision": "682d2df7621fe4ab3c0b",
+    "url": "/js/chunk-vendors.8d25b6d7.js"
+  },
+  {
+    "revision": "479f5022008b8d4b6ef6f2d406ead5cf",
+    "url": "/js/core/bootstrap-material-design.min.js"
+  },
+  {
+    "revision": "986d2baef41aa37ae02ab33355413b98",
+    "url": "/js/core/jquery.min.js"
+  },
+  {
+    "revision": "10a554dd975faf4004fc557d7cf8c998",
+    "url": "/js/core/popper.min.js"
+  },
+  {
+    "revision": "140402aaa6b688b19f414c6cdc5a71b5",
+    "url": "/js/material-kit.js"
+  },
+  {
+    "revision": "260d912aaf83fecdea077916d9c9b18b",
+    "url": "/js/material-kit.min.js"
+  },
+  {
+    "revision": "14382219d09a6c2272e95a95a6bbc5c8",
+    "url": "/js/plugins/bootstrap-datetimepicker.js"
+  },
+  {
+    "revision": "9901cb7dc7a9bce08e8f37fa38891012",
+    "url": "/js/plugins/jquery.sharrre.js"
+  },
+  {
+    "revision": "234c4f6c148398f299e8ba0911e2fc3b",
+    "url": "/js/plugins/moment.min.js"
+  },
+  {
+    "revision": "e22ce95f494c1b97c1215213c03fcd75",
+    "url": "/js/plugins/nouislider.min.js"
+  },
+  {
+    "revision": "4605efb74309679d68949eab001b4fe7",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9e75559463874f4a3298e691903014e4.js"
+  "/precache-manifest.2d2a52d7992f730f93d520434d1fd51e.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "sosconf"});
